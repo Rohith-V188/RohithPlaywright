4922b9994a92c1208a590cb4e62011b533ffd803
Rohith-V188 <rohith.vijay188@gmail.com>
2025-12-10 16:27:15 +0530

    Spec changes

--- tests/login.spec.js
+++ tests/login.spec.js
@@ -21,3 +21,16 @@ test("Verifying Login page", async ({page}) =>
         await expect (page.locator("(//p[text()='Invalid credentials']")).toBeVisible('Invalid credentials')
 
     })
+
+    test('verify swaglabs login page and logout', async ({ page }) => {
+  await page.goto('https://opensource-demo.orangehrmlive.com/web/index.php/auth/login');
+  await page.getByRole('textbox', { name: 'Username' }).click();
+  await page.getByRole('textbox', { name: 'Username' }).fill('Admin');
+  await page.getByRole('textbox', { name: 'Password' }).click();
+  await page.getByRole('textbox', { name: 'Password' }).fill('admin123');
+  await page.getByRole('button', { name: 'Login' }).click();
+  await expect(page.getByRole('heading', { name: 'Dashboard' })).toBeVisible();
+  await page.getByRole('listitem').filter({ hasText: 'Test User' }).locator('i').click();
+  await page.getByRole('menuitem', { name: 'Logout' }).click();
+  await expect(page.getByRole('heading', { name: 'Login' })).toBeVisible();
+});
